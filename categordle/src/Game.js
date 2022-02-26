6d85be3304808201dec05185bfdfdeaf2489aa2e
Anais <anais.levi20@gmail.com>
2022-02-26 17:36:58 -0500

    Display words on new lines

--- categordle/src/Game.js
+++ categordle/src/Game.js
@@ -1,10 +1,6 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 
-
-
-
-
 export default function Game() {
     const [words, setWords] = useState([]);
     const [answer, setAnswer] = useState([]);
@@ -13,19 +9,19 @@ export default function Game() {
     const allWordMatches = findMatches(answer);
     const [currentWordMatchIndex, setCurrentWordMatchIndex] = useState(1);
     const [displayedWordMatches, setDisplayedWordMatches] = useState([allWordMatches.at(0)])
-  
-  
-  //TODO: generate random answer word
-  const getAnswerIndex = () => Math.floor(Math.random() * 4);
+
+
+    //TODO: generate random answer word
+    const getAnswerIndex = () => Math.floor(Math.random() * 4);
     // Math.floor( 
     //   (Date.now() - new Date(2022, 0, 23, 0, 0, 0).getTime()) / 86400e3
     // ) % NUM_ANSWERS;
-  
-  
-  //TODO: MAITE give me word associations
-  function findMatches(word) {
-    return ["word1", "word2", "word3", "word4", "word5", "word6"];
-  }
+
+
+    //TODO: MAITE give me word associations
+    function findMatches(word) {
+        return ["word1", "word2", "word3", "word4", "word5", "word6"];
+    }
     console.log(answer);
 
     const updateDisplayedWords = () => {
@@ -34,12 +30,15 @@ export default function Game() {
     }
 
 
-  return (
-    <div>
-    Current words: {displayedWordMatches}
-    Index: {currentWordMatchIndex}
-    <hr />
-    <button onClick={updateDisplayedWords}> </button>
-  </div>
-  );
+    return (
+        <div>
+            Current words: {displayedWordMatches.map((word) => <><br/>{word}</>)}
+            <br></br>           
+            <label>Enter your guess:
+                <input type="text" />
+                <button onClick={updateDisplayedWords}>Guess!</button>
+            </label>
+            
+        </div>
+    );
 }
