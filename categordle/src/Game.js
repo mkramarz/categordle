d58b64192208e6472a614e246a561633719cdcc1
mkramarz <87330714+mkramarz@users.noreply.github.com>
2022-02-26 17:10:57 -0500

    Implements findMatches() as an async

--- categordle/src/Game.js
+++ categordle/src/Game.js
@@ -1,9 +1,7 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-
-
-
-
+import * as util from "util";
+const w2v = require('word2vec');
 
 export default function Game() {
     const [words, setWords] = useState([]);
@@ -20,12 +18,37 @@ export default function Game() {
     // Math.floor( 
     //   (Date.now() - new Date(2022, 0, 23, 0, 0, 0).getTime()) / 86400e3
     // ) % NUM_ANSWERS;
-  
-  
-  //TODO: MAITE give me word associations
-  function findMatches(word) {
-    return ["word1", "word2", "word3", "word4", "word5", "word6"];
-  }
+
+
+    const loadModelPromise = util.promisify(w2v.loadModel);
+
+    async function findMatches(theme) {
+        let toReturn = [];
+        let model = await loadModelPromise('./config/wordvecs.txt');
+        const similarWords = model.mostSimilar(theme, 15);
+        for (let wordPair of similarWords) {
+            const word = wordPair['word'].toLowerCase();
+            if (toReturn.length === 6) { //Once we have enough words, we can stop early
+                break;
+            }
+            if (word.includes(theme)) { //We'll toss out any words that are too similar
+                continue;
+            }
+            let matchesExisting = false;
+            for (let existingWord of toReturn) { //But clues also shouldn't be too similar to each other
+                if (existingWord.includes(word) || word.includes(existingWord)) {
+                    matchesExisting = true;
+                    break;
+                }
+            }
+            if (matchesExisting) {
+                continue;
+            }
+            toReturn.push(word);
+        }
+        toReturn.reverse(); //Our clues should be in reverse order!
+        return toReturn;
+    }
     console.log(answer);
 
     const updateDisplayedWords = () => {
