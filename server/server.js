4270b34f6a808a4e1acbf98c98fc9bc4381da991
mkramarz <87330714+mkramarz@users.noreply.github.com>
2022-02-27 09:36:41 -0500

    Updates API to be functional and not require any message-sending

--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
+import {createRequire} from "module"
+const require = createRequire(import.meta.url);
+
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 3001;
+
+import {commonWords} from './words.js'
 
 const util = require('util');
 const w2v = require('word2vec');
+
+function pickWord() {
+    let index = Math.floor(Math.random() * commonWords.length);
+    return commonWords[index];
+}
 
 const loadModelPromise = util.promisify(w2v.loadModel);
 
 async function findMatches(theme) {
     let toReturn = [];
-    let model = await loadModelPromise('./config/wordvecs.txt');
+    let model = await loadModelPromise('/home/maite/WebstormProjects/categordle2/server/wordvecs.txt');
     const similarWords = model.mostSimilar(theme, 15);
     for (let wordPair of similarWords) {
         const word = wordPair['word'].toLowerCase();
@@ -35,9 +45,14 @@ async function findMatches(theme) {
     return toReturn;
 }
 
-app.get('/', (req, res) => {
-    findMatches(req).then((hints) => {
-        res.send(hints);
+app.get('/api', (req, res) => {
+    let word = pickWord();
+    const wordAndHints = [word];
+    findMatches(word).then((hints) => {
+        for (let hint of hints) {
+            wordAndHints.push(hint);
+        }
+        res.send(wordAndHints);
     });
 });
 
